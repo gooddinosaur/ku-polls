602541efe36e2f2262ed937fb64d8f19ddc25874
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-08-30 22:33:49 +0700

    Added an end_date attribute and Added a default for pub_date in models.py

--- polls/models.py
+++ polls/models.py
@@ -12,12 +12,20 @@ from django.utils import timezone
 class Question(models.Model):
     """ Represents a poll question. """
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    end_date = models.DateTimeField('end date', null=True, blank=True)
 
     def was_published_recently(self):
         """ Returns True if the question was published within the last day. """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_voting_allowed(self):
+        """ Returns True if voting is allowed. """
+        now = timezone.now()
+        if self.end_date is None:
+            return now >= self.pub_date
+        return self.pub_date <= now <= self.end_date
 
     def __str__(self):
         return self.question_text
