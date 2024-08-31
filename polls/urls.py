43844934731bbc2f67300a70001546c4ba445e71
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-08-31 12:07:33 +0700

    Modify code for redirecting from http://localhost:8000/ to  http://localhost:8000/polls/ in mysite/urls.py and polls/urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-
+from django.views.generic.base import RedirectView
 from . import views
 
 app_name = 'polls'
+
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', RedirectView.as_view(url='/polls/', permanent=True),
+         name='redirect_to_polls'),
+    path('polls/', views.IndexView.as_view(), name='index'),
+    path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('polls/<int:pk>/results/', views.ResultsView.as_view(),
+         name='results'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
